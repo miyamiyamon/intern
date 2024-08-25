ff0c988d9d3f50633c543fbd0affcee87a0c6c9b
miyamon <r118168@g.kobe-kosen.ac.jp>
2024-08-26 07:15:43 +0900

    totyuukeika

---
+++ SideChannel/Assets/RegisterStringLogSideChannel.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.SideChannels;
+
+public class RegisterStringLogSideChannel : MonoBehaviour
+{
+    StringLogSideChannel stringChannel;
+
+    // ゲームオブジェクトの生成時に呼ばれる (1)
+    public void Awake()
+    {
+        // サイドチャネルの生成
+        stringChannel = new StringLogSideChannel();
+
+        // サイドチャネルの登録
+        SideChannelManager.RegisterSideChannel(stringChannel);
+    }
+
+    // ゲームオブジェクトの破棄時に呼ばれる
+    public void OnDestroy()
+    {
+        // サイドチャネルの登録を解除
+        if (Academy.IsInitialized)
+        {
+            SideChannelManager.UnregisterSideChannel(stringChannel);
+        }
+    }
+
+    // フレーム毎に呼ばれる
+    public void Update()
+    {
+        // スペースキーを押した時 (3)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Pythonへのデータの送信
+            stringChannel.SendString("Send Data from Unity");
+        }
+    }
+}
---
+++ SideChannel/Assets/StringLogSideChannel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.SideChannels;
+using System.Text;
+using System;
+
+public class StringLogSideChannel : SideChannel // (1)
+{
+    // コンストラクタ
+    public StringLogSideChannel()
+    {
+        // ChannelIDの指定 (2)
+        ChannelId = new Guid("621f0a70-4f87-11ea-a6bf-784f4387d1f7");
+    }
+
+    // Python側からのデータの受信 (3)
+    protected override void OnMessageReceived(IncomingMessage msg)
+    {
+        Debug.Log(msg.ReadString());
+    }
+
+    // Python側へのデータの送信 (4)
+    public void SendString(string sendString)
+    {
+        using (var msgOut = new OutgoingMessage())
+        {
+            msgOut.WriteString(sendString);
+            QueueMessageToSend(msgOut);
+        }
+    }
+}
